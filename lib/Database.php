ffc677dc8f5cf1cbb12f919cdab36f98d634dfea
Nelson Belgarde <nelsonbelgarde@gmail.com>
2021-11-30 09:00:28 +0100

    Create Database class and create database

---
+++ lib/Database.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("_connect.php");
+
+class Database
+{
+    private static ?Database $instance = null;
+    
+    private PDO $pdo;
+
+    private function __construct()
+    {
+        $this->pdo = new PDO("mysql:host=".DATABASE_HOST.";dbname=".DATABASE_NAME, DATABASE_USERNAME, DATABASE_PASS);
+    }
+
+    public static function getInstance(): Database
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        
+        return self::$instance;
+    }
+
+    public function query(string $query, array $params = []): array
+    {
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($params);
+        
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function listTable(string $tableName): array
+    {
+        return $this->query('SELECT * FROM '.$tableName);
+    }
+}
