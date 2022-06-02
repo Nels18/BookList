19b5bcf649c1079f6205c0996b88498801e2230c
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-06-02 23:01:12 +0200

    feat: changed the access to the properties

--- src/View/book/index.php
+++ src/View/book/index.php
@@ -2,7 +2,7 @@
 <?php foreach ($books as $book): ?>
 <div class="card p-3 my-5">
     <div class="card-body">
-        <a href="?p=book/show/<?= $book->id ?>"><h5 class="card-title"><?= $book->title; ?></h5></a>
+        <a href="?p=book/show/<?= $book['id'] ?>"><h5 class="card-title"><?= $book['title']; ?></h5></a>
     </div>
 </div>
 <?php endforeach; ?>
