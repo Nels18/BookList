0325661077d0a92937c9c8d3d2c920b3d3023011
Nelson Belgarde <nelsonbelgarde@gmail.com>
2022-06-04 03:01:26 +0200

    feat: improved style and removed cancel buttuon

--- src/View/book/add.php
+++ src/View/book/add.php
@@ -1,11 +1,2 @@
-<?php
-    // use App\Controller\BookController;
-
-    // $book = new BookController();
-    // $book->add();
-?>
-<h2>Ajouter un livre</h2>
+<h2 class="my-4">Ajouter un livre</h2>
 <?= $bookForm ?>
-<div class="col-md-4 my-3 mx-auto">
-    <a href="/?p=book/index"><button class="btn btn-outline-danger w-100">Annuler</button></a>
-</div>
--- src/View/book/edit.php
+++ src/View/book/edit.php
@@ -1,5 +1,2 @@
-<h2>Modifier un livre</h2>
+<h2 class="my-4">Modifier un livre</h2>
 <?= $bookForm ?>
-<div class="col-md-4 my-3 mx-auto">
-    <a href="/?p=book/index"><button class="btn btn-outline-danger w-100"><?= isset($book) ? 'Annuler la modification' : 'Annuller l\'ajout' ?></button></a>
-</div>
